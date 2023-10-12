43370365bd57b5e6b60fed723e110ae6a500edd5
dinesh kumar <chayaldinesh1@gmail.com>
2023-10-12 17:41:55 +0530

    added text

--- FoodMart Frontend/src/app/components/home/home.component.ts
+++ FoodMart Frontend/src/app/components/home/home.component.ts
@@ -1,76 +1,53 @@
-import { Component } from '@angular/core';
-import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
-import { Restaurant } from 'src/app/model/restaurant/restaurant';
-import { RestaurantService } from 'src/app/service/restaurant.service';
+import {Component} from '@angular/core';
+import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from '@angular/material/snack-bar';
+import {Router} from '@angular/router';
+import {Restaurant} from 'src/app/model/restaurant/restaurant';
+import {RestaurantService} from 'src/app/service/restaurant.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: 'app-home', templateUrl: './home.component.html', styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
-  constructor(private router:Router,private snackBar:MatSnackBar,private restaurantService:RestaurantService){
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  searchText: string = "";
+  allRestaurant: any;
+
+
+
+  constructor(private router: Router, private snackBar: MatSnackBar, private restaurantService: RestaurantService) {
     this.getAllRestaurant();
   }
 
-    
-horizontalPosition: MatSnackBarHorizontalPosition = 'center';
-verticalPosition: MatSnackBarVerticalPosition = 'top';
-
-searchText:string = "";
-
-  // search(){
-  //   this.snackBar.open("You need to login first","Ok",{
-  //     horizontalPosition:this.horizontalPosition,
-  //     verticalPosition:this.verticalPosition,
-  //   });
-  //   this.router.navigateByUrl("/userLogin");
-  // }
-
-  clear(){
-    this.searchText= '';
+  clear() {
+    this.searchText = '';
     this.getAllRestaurant();
   }
 
-  search(){
+
+
+
+  search() {
     console.log(this.searchText);
-    if(this.searchText == null){
+    if (this.searchText == null) {
       this.getAllRestaurant();
-    }
-    else{
-      this.allRestaurant= this.allRestaurant.filter((data: { restaurantName: string; }) => data.restaurantName?.startsWith(this.searchText));
+    } else {
+      this.allRestaurant = this.allRestaurant.filter((data: {
+        restaurantName: string;
+      }) => data.restaurantName?.startsWith(this.searchText));
       console.log(this.allRestaurant);
       // alert("test")
     }
   }
-  
-  
-  // search() {
-  //   if (this.searchText === '' || !this.searchText)
-  //   this.getAllRestaurant();
-  // else {
-  //   // this.allRestaurant = this.allRestaurant.filter(data => notes.title?.startsWith(this.searchText.toLowerCase()));
-  //   this.allRestaurant = this.allRestaurant.filter((restaurantName => restaurantName.title?. startsWith(this.searchText.toLowerCase())));
-  // }
 
-  // }
-
-
-
-
-  allRestaurant:any;
-
-  getAllRestaurant(){
-    this.restaurantService.getRestaurant().subscribe(
-      response=>{
-        this.allRestaurant = response as Restaurant[];
-        console.log(response);
-      }
-    ),(error: any)=>{
+  getAllRestaurant() {
+    this.restaurantService.getRestaurant().subscribe(response => {
+      this.allRestaurant = response as Restaurant[];
+      console.log(response);
+    }), (error: any) => {
       console.log(error)
     }
 
-}
+  }
 }
